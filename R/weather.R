527258a18ca70da3074bbe73d125cfa84e91f69f
Kevin Wolz <wolzkevin@gmail.com>
2018-03-28 10:56:52 +0200

    VERSION UPDATE
    Added functions to read in and write out Hi-sAFe weather files. More sophisticated modifcation of weather files is such a custom and project-specific activity, and weather files are such a simple data frame, that no further weather functions are deemed necessary at this time.
    
    Fixes #37

---
+++ R/weather.R
@@ -0,0 +1,45 @@
+#' Read weather data from a Hi-sAFe .WTH file.
+#' @description Reads weather data from a Hi-sAFe .WTH file.
+#' @return A data frame (tibble) of the weather data.
+#' @param path A character string of the path (relative or absolute) to the .WTH file.
+#' @export
+#' @family hisafe weather functions
+#' @examples
+#' \dontrun{
+#' wth <- read_weather("./my_site.wth")
+#' }
+read_weather <- function(path) {
+  if(!file.exists(path)) stop("file specfied by path does not exist", call. = FALSE)
+  wth <- dplyr::as_tibble(read.table(file             = path,
+                                     header           = FALSE,
+                                     sep              = "\t",
+                                     stringsAsFactors = FALSE,
+                                     comment.char     = "#",
+                                     encoding         = "latin1"))
+  names(wth) <- c("doy", "year", "month", "day", "Tmax", "Tmin", "RHmax", "RHmin", "Rg", "precip", "wind", "watertable", "CO2")
+  return(wth)
+}
+
+#' Write weather data to a Hi-sAFe .WTH file.
+#' @description Writes weather data to a Hi-sAFe .WTH file.
+#' @param data A data frame containing the weather data.
+#' @param path A character string of the path (relative or absolute) to the .WTH file.
+#' @export
+#' @family hisafe weather functions
+#' @examples
+#' \dontrun{
+#' write_weather(wth, "./my_site_modified.wth")
+#' }
+write_weather <- function(data, path) {
+  required.names <- c("doy", "year", "month", "day", "Tmax", "Tmin", "RHmax", "RHmin", "Rg", "precip", "wind", "watertable", "CO2")
+
+  if(!any(c("tbl", "data.frame") %in% class(data))) stop("data must be of class data.frame or tibble",                                    call. = FALSE)
+  if(!identical(names(data), required.names))       stop(paste("names of data columns must be:", paste(required.names, collapse = ", ")), call. = FALSE)
+  if(!(str_sub(path, -3, -1) %in% c("WTH", "wth"))) stop("path must point to a file with extension .wth or .WTH",                         call. = FALSE)
+  if(file.exists(path))                             stop("file specfied by path already exists",                                          call. = FALSE)
+
+  readr::write_delim(x         = data,
+                     path      = path,
+                     delim     = "\t",
+                     col_names = FALSE)
+}
