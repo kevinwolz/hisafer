4368d09776784923b855815461bfc37c21dbda51
Kevin Wolz <wolzkevin@gmail.com>
2018-07-02 20:47:08 +0200

    FIxed bug in diag_hisafe_ts() so that last.col was specified for cells.

--- R/diagnostics.R
+++ R/diagnostics.R
@@ -32,7 +32,8 @@ diag_hisafe_ts <- function(hop, profile, output.path = NULL, ...) {
   ts.path <- clean_path(paste0(diag_output_path(hop = hop, output.path = output.path), "/diagnostics/", profile, "/"))
   dir.create(ts.path, recursive = TRUE, showWarnings = FALSE)
 
-  last.col  <- ifelse(grepl("tree", profile), "id", "stepNum")
+  if(profile == "tree") last.col <- "id" else if(profile == "cells") last.col <- "y" else last.col <- "stepNum"
+
   var.names <- vars_to_diag(hop = hop, profile = profile, last.col = last.col)
   plot.list <- purrr::map(var.names, plot_hisafe_ts, hop = hop, profile = profile, ...)
 
