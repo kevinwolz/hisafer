5adf4bee106367aca6246c42fd23e70984333216
Kevin Wolz <wolzkevin@gmail.com>
2018-10-04 20:57:54 +0200

    Update vignette

--- inst/doc/hisafer-vignette.R
+++ inst/doc/hisafer-vignette.R
@@ -104,25 +104,32 @@ hip <- define_hisafe(path      = "./simulations",
 #             parallel    = TRUE,
 #             capsis.path = "/Applications/Capsis/")
 
-## ---- eval = FALSE-------------------------------------------------------
-#  hop <- read_hisafe(hip      = hip,
-#                     profiles = c("plot", "trees", "climate", "monthCells", "cells", "voxels"))
+## ---- echo = FALSE, eval = FALSE-----------------------------------------
+#  example.path <- "/Users/kevinwolz/Desktop/RESEARCH/ACTIVE_PROJECTS/HI-SAFE/hisafer/inst/extdata/example_exp"
+#  example.profiles <- c("plot", "plotDetail", "trees", "treesDetail", "cells",  "cellsDetail",
+#                        "voxelsMonth", "climate", "monthCells", "annualCells")
+#  mc.hip <- define_hisafe(path = example.path, template = "monocrop"    , SimulationName = "monocrop",     profiles = example.profiles, sticsReport = 1)
+#  af.hip <- define_hisafe(path = example.path, template = "agroforestry", SimulationName = "agroforestry", profiles = example.profiles)
+#  pf.hip <- define_hisafe(path = example.path, template = "forestry",     SimulationName = "forestry",     profiles = example.profiles)
+#  build_hisafe(hip = mc.hip)
+#  build_hisafe(hip = af.hip)
+#  build_hisafe(hip = pf.hip)
+#  build_cluster_script(simu.names    = c("monocrop", "agroforestry", "forestry"),
+#                       hip           = NULL,
+#                       script.path   = example.path,
+#                       cluster.path  = paste0("/lustre/lecomtei/tests/example_exp/"),
+#                       email.type    = "NONE")
 
-## ---- echo = FALSE-------------------------------------------------------
-#hisafer_example_hop <- read_hisafe(path       = "/Users/kevinwolz/Desktop/RESEARCH/ACTIVE_PROJECTS/HI-SAFE/vignette_output/lue", 
-#                                   simu.names = c("Sim_1", "Sim_2"), 
-#                                   profiles   = c("plot", "trees", "climate", "monthCells", "cells", "voxels"))
-#save(hisafer_example_hop, file = "/Users/kevinwolz/Desktop/hisafer_example_hop.Rdata")
-#devtools::use_data(hisafer_example_hop)
-hop  <- hisafer_example_hop
+## ------------------------------------------------------------------------
+hop.raw <- hop <- read_hisafe_example()
 
-## ---- eval = FALSE-------------------------------------------------------
-#  hop$plot
+## ---- eval = TRUE--------------------------------------------------------
+hop$metadata
 
 ## ------------------------------------------------------------------------
 hop <- hop_rename(hop       = hop,
-                  old.names = paste0("Sim_", 1:2),
-                  new.names = paste0("lue-", hop$exp.plan$lueMax))
+                  old.names = c("monocrop", "agroforestry", "forestry"),
+                  new.names = c("MC", "AF", "PF"))
 
 ## ------------------------------------------------------------------------
 hop <- hop_filter(hop        = hop,
@@ -142,7 +149,7 @@ plot_hisafe_monthcells(hop        = hop,
                        variable   = "monthRelativeDirectParIncident",
                        colfacet   = "Year",
                        rowfacet   = "SimulationName",
-                       years      = 2:9,
+                       years      = 1995:2003,
                        months     = 7)
 
 ## ---- eval = TRUE, dpi = 250, fig.width = 10-----------------------------
@@ -169,34 +176,58 @@ plot_hisafe_cycle_daily(hop = hop, cycle = "water", years = 2003)
 ## ---- eval = TRUE, dpi = 250, fig.width = 10-----------------------------
 plot_hisafe_cycle_daily(hop = hop, cycle = "light", years = 2003)
 
-## ---- dpi = 250, fig.width = 20------------------------------------------
-hisafe_slice(hop  = hop,
-             date = "2003-06-01")
+## ---- eval = TRUE, dpi = 250, fig.width = 10-----------------------------
+plot_hisafe_cycle_daily(hop = hop, cycle = "carbon-increment", years = 2003)
+
+## ---- eval = TRUE, dpi = 250, fig.width = 10-----------------------------
+plot_hisafe_cycle_daily(hop = hop, cycle = "carbon-allocation", years = 2003)
+
+## ---- eval = TRUE, dpi = 250, fig.width = 10-----------------------------
+summary.out <- cycle_summary(hop         = hop,
+                             daily.year  = 2003,
+                             cycles      = c("carbon", "light", "water", "nitrogen"),
+                             simu.name   = "AF",
+                             crop.names  = c("Crop", "Tree line vegetation"))
+summary.out
 
 ## ---- dpi = 250, fig.width = 20------------------------------------------
-hisafe_slice(hop    = hop,
-             plot.x = "y",
-             date   = "2003-06-01")
+hisafe_slice(hop        = hop,
+             simu.names = c("AF", "PF"),
+             date       = "1999-06-01")
+
+## ---- dpi = 250, fig.width = 20------------------------------------------
+hisafe_slice(hop        = hop,
+             simu.names = c("AF", "PF"),
+             date       = "2003-07-01")
 
 ## ---- dpi = 250, fig.width = 8-------------------------------------------
 visual_legend(hop = hop)
 
-## ---- echo = FALSE, eval = FALSE-----------------------------------------
-#  #load(paste0(system.file("extdata", "vignette_output", package = "hisafer"), "/vignette_face.Rdata"))
-#  face <- hisafer_example_face
-
 ## ---- eval = FALSE-------------------------------------------------------
-#  face <- create_face(agroforestry = AF.hop,
-#                      forestry     = FC.hop,
-#                      monocrop     = CC.hop,
-#                      face.path    = "./simulations")
+#  hisafe_root3D(hop      = hop,
+#               simu.name = "AF",
+#               date      = "2003-06-01")
 
-## ---- eval = FALSE-------------------------------------------------------
-#  LER(face = face)
+## ---- eval = TRUE--------------------------------------------------------
+face <- create_face(agroforestry = hop_filter(hop, "AF"), 
+                    forestry     = hop_filter(hop, "PF"), 
+                    monocrop     = hop_filter(hop, "MC"), 
+                    face.path    = ".")
+
+## ---- eval = TRUE, dpi = 250, fig.width = 10-----------------------------
+LER(face = face)
+
+## ---- eval = TRUE, dpi = 250, fig.width = 10-----------------------------
+LER(face = face, cycle = "light")
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  plot_hisafe_cycle_annual(hop = face, cycle = "carbon")
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  plot_hisafe_cycle_annual(hop = face, cycle = "light")
+
+## ---- eval = TRUE, dpi = 250, fig.width = 10-----------------------------
+ler.out <- LER_summary(face = face)
+
+ler.out
 
